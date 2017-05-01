6ba88152dfb6d701077b1de5fe26f8b676c0a9d0
mikeyjin <mayatdr@gmail.com>
2017-05-01 16:47:18 +0300

    minor changes

--- screaping.py
+++ screaping.py
@@ -1,9 +1,41 @@
 #this is a a simple script for crawling
+
+#importing libs
 import bs4 as bs
 import urllib.request
+import sys
 
-sauce = urllib.request.urlopen('http://benyehuda.org').read()
+#opening url, making soup with bs4
+sauce = urllib.request.urlopen('http://benyehuda.org/bialik/').read()
 soup = bs.BeautifulSoup(sauce, "lxml")
-allPs = soup.find_all('p')
-for paragraph in allPs:
-    print(paragraph.text)
+
+#find requested song
+a = soup.find("a",text = "אל הצפור")
+
+
+#creating inner link for the song
+newlink = 'http://benyehuda.org/bialik/' + a['href']
+
+#TEST:
+#print(newlink)
+
+#opening song url and making soup
+sauce = urllib.request.urlopen(newlink).read()
+soup = bs.BeautifulSoup(sauce, "lxml")
+#find lines of the song
+ps = soup.find_all(attrs={"class":"a3"})
+dict = {'א':"⠁"}
+
+#cleaning NIKUD!
+i=0
+for p in ps:
+    string = list(p.text)
+    while i<len(string):
+        if not(ord(string[i])>=1488 and ord(string[i])<=1514 or string[i]==' ' ):
+            del(string[i])
+        i+=1
+
+#writing into file
+i=0
+with open("text.txt", "w") as f:
+        f.write(str(ord(dict['א'])))
