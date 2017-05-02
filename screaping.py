391dfeec2131c6f74fbe73156027df61f6f96c00
mikeyjin <mayatdr@gmail.com>
2017-05-02 11:53:19 +0300

    added a full braille dictionary

--- screaping.py
+++ screaping.py
@@ -5,11 +5,11 @@ import bs4 as bs
 import urllib.request
 import sys
 
-#opening url, making soup with bs4
+#opening url, making soup with bs4 - (poet should be received from user)
 sauce = urllib.request.urlopen('http://benyehuda.org/bialik/').read()
 soup = bs.BeautifulSoup(sauce, "lxml")
 
-#find requested song
+#find requested song - (name should be received from user)
 a = soup.find("a",text = "אל הצפור")
 
 
@@ -22,11 +22,14 @@ newlink = 'http://benyehuda.org/bialik/' + a['href']
 #opening song url and making soup
 sauce = urllib.request.urlopen(newlink).read()
 soup = bs.BeautifulSoup(sauce, "lxml")
-#find lines of the song
+#find the headline of the song
 ps = soup.find_all(attrs={"class":"a3"})
 
 #dictionary for braille
-dict = {'א':'⠁'}
+dict = {'א':'⠁','ב':'⠃','ג':'⠛','ד':'⠙','ה':'⠓','ו':'⠺','ז':'⠵',\
+        'ח':'⠭','ט':'⠞','י':'⠚','כ':'⠅','ל':'⠇','מ':'⠍','נ':'⠝',\
+        'ס':'⠎','ע':'⠫','פ':'⠏','צ':'⠮','ק':'⠟','ר':'⠗','ש':'⠩','ת':'⠹',\
+        ' ':' ','\r':'\r',}
 
 #cleaning NIKUD!
 i=0
@@ -37,12 +40,12 @@ for p in ps:
             del(string[i])
         i+=1
 
+#making new list with Braille chars.
 braileList = []
 for char in string:
-    braileList[char] = dict[char]
+    braileList.append(dict[char])
 
-#writing into file (hallelujah!!! took me a lot of time to figure out the encoding...\=)
-
+#writing into file (hallelujah!!! took me a lot of time to figure out the encoding stuff...\=)
 with open("text.txt", "w",encoding='utf-8') as f:
     f.write(''.join(braileList))
     #print(dict['א'])
