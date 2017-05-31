9d1bb1ad369064d62f626483e684dec0404acbe1
Ayelet Kalderon <ayelet.kalderon@cymotive.com>
2017-06-01 01:21:21 +0300

    added stanza to tei

--- tei_parser.py
+++ tei_parser.py
@@ -1,8 +1,6 @@
 import sys
 import os
-# import re
 import MySQLdb
-# import csv
 import codecs
 import unicodedata
 import dicttoxml
@@ -10,12 +8,12 @@ import collections
 
 #sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer, 'strict')
 
-# print('hello, Yul')
+print('hello, Yul')
 
 
 myDB = MySQLdb.connect(host="tdh.cmq2zbutzn8e.us-west-2.rds.amazonaws.com",port=3306,user="bialik",passwd="12345678",db="tdh172",charset='utf8')
 cHandler = myDB.cursor()
-cHandler.execute("SELECT poems.name,original_data,wikipedia_name,poems.id, year_of_birth, year_of_death from poems JOIN poets ON poet_id = poets.id")
+cHandler.execute("SELECT poems.name,original_data,wikipedia_name,poems.id, year_of_birth, year_of_death from poems JOIN poets ON poet_id = poets.id LIMIT 3")
 poems_data = cHandler.fetchall()
 
 for poem in poems_data:
@@ -55,8 +53,8 @@ for poem in poems_data:
 			return "l"
 
 
-	tei_file = dicttoxml.dicttoxml(json_data, attr_type=False, item_func=item_func, root=False).decode('utf8')
-	tei_file = tei_file.replace('<lg>', '<lg type="stanza">')
+	tei_file = dicttoxml.dicttoxml(json_data, attr_type=False, item_func=item_func).decode('utf8')
+	tei_file = tei_file.replace('<lg >', '<lg type="stanza">')
 	tei_file = tei_file.replace('<body>', '<body xml:id="d2">')
 	tei_file = tei_file.replace('<text>', '<text xml:id="d1">')
 	tei_file = tei_file.replace('<div1>', '<div1 type="poem" xml:id="d3">')
@@ -67,10 +65,9 @@ for poem in poems_data:
 
 	# cHandler.execute("UPDATE poems set tei=tei_file where id=poem_id;")
 	# myDB.commit()
-	# with open(r"C:\temp\example2.tei", "w", encoding='utf-8') as f:
-	# 	f.write(tei_file)
+	with open(r"C:\temp\example2.tei", "w", encoding='utf-8') as f:
+		f.write(tei_file)
 	
 	#print(cleanNikudFromString(cleanSpecialCharsFromString(poem[1])))
-
 
 myDB.close()
